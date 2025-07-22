66bee50d1aefc6a8853e4b6dabfc9f4ea78609de
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-21 21:35:21 -0500

    isalnum_python
    isalnum_python

---
+++ is/ejemplo.py
@@ -0,0 +1,6 @@
+# Se crea una variable llamada 'letras_numeros' y se le asigna la cadena de texto "julianvelez19"
+letras_numeros = "julianvelez19"
+
+# Se utiliza el método .isalnum() para verificar si todos los caracteres de la cadena son alfanuméricos
+# Es decir, que solo contenga letras (a-z, A-Z) y/o números (0-9), sin espacios ni símbolos especiales
+print(letras_numeros.isalnum())  # Esto imprimirá True porque "julianvelez19" solo contiene letras y números
